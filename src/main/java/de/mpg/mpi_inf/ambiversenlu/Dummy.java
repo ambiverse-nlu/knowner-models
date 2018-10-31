cb14f0146797e5cc6f7d80bbcbd21ebfe31ee448
Dragan Milchevski <dragan@ambiverse.com>
2018-10-31 10:49:34 +0100

    Adding dummy class to generate java-doc needed to perform the release

---
+++ src/main/java/de/mpg/mpi_inf/ambiversenlu/Dummy.java
@@ -0,0 +1,29 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.mpg.mpi_inf.ambiversenlu;
+
+/**
+ * This project contains the KnowNER models needed to perform the Named Entity Recognition by the Entity Linking project,
+ * as a part of the AmbiverseNLU codebase @see <a href="https://github.com/ambiverse-nlu/">https://github.com/ambiverse-nlu</a>.
+ *
+ * @since 1.0
+ * @author AmbiverseNLU
+ */
+public class Dummy {
+
+}
